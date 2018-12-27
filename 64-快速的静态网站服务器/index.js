86acf017b41cc2b1434aa01acbe546aa21f436d5
nemo <nemo@nemodeMacBook-Pro.local>
2018-12-27 10:28:22 +0800

    add

---
+++ 64-快速的静态网站服务器/index.js
@@ -0,0 +1,30 @@
+var connect = require('connect');
+var http = require('http');
+var untils = require('./untils');
+var app = connect();
+ 
+
+const host = untils.getIPAdress();
+// gzip/deflate outgoing responses
+var compression = require('compression');
+app.use(compression());
+ 
+// store session state in browser cookie
+var cookieSession = require('cookie-session');
+app.use(cookieSession({
+    keys: ['secret1', 'secret2']
+}));
+ 
+// parse urlencoded request bodies into req.body
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+ 
+// respond to all requests
+app.use(function(req, res){
+  res.end('Hello from Connect!\n');
+});
+
+//create node.js http server and listen on port
+http.createServer(app).listen(3000, function () {
+    console.log(host);
+});
